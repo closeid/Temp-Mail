3f1334dafb1bb61cf927c54c3c736139c552b236
closeid <changdong.deng@gmail.com>
2026-07-01 00:33:22 +0800

    Upload Files

--- frontend/src/views/Footer.vue
+++ frontend/src/views/Footer.vue
@@ -1,9 +1,5 @@
 <script setup>
 import { useScopedI18n } from '@/i18n/app'
-import { useGlobalState } from '../store'
-import DOMPurify from 'dompurify'
-const { openSettings } = useGlobalState()
-
 
 const { t } = useScopedI18n('views.Footer')
 
@@ -16,9 +12,6 @@ const { t } = useScopedI18n('views.Footer')
             <n-space justify="center">
                 <n-text depth="3">
                     {{ t('copyright') }} © 2023-2026 GetAnEmail
-                </n-text>
-                <n-text depth="3">
-                    <div v-html="DOMPurify.sanitize(openSettings.copyright)"></div>
                 </n-text>
             </n-space>
         </div>
